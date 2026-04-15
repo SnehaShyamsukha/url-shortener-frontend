9ca3ddc972eead4dc2994c0f67060dadbab72a9d
Sneha <snehashyamsukha29@gmail.com>
2026-04-15 16:58:20 +0530

    Fixed API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: "https://url-shortener-backend-deo7.onrender.com"
 });
 
 // AUTH HEADER AUTO
